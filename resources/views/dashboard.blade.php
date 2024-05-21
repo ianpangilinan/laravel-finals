e5247aafa8d10ec5de063cb8bc74a029d46d8f8d
Ian <iannpangilinan@gmail.com>
2024-05-21 15:34:28 +0100

    Dashboard (Total numbers of Post, Publised, Unpublised) Successfully Implement to the project

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,87 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+
+                    <div class="pagetitle">
+                        <h1>Dashboard</h1>
+                        <nav>
+                            <ol class="breadcrumb">
+                                <li class="breadcrumb-item active">Dashboard</li>
+                            </ol>
+                        </nav>
+                    </div><!-- End Page Title -->
+
+                    <section class="section dashboard">
+                        <div class="row">
+
+                            <!-- Left side columns -->
+                            <div class="col-lg-8">
+                                <div class="row">
+
+                                    <!-- Sales Card -->
+                                    <div class="col-xxl-4 col-md-6">
+                                        <div class="card info-card sales-card">
+                                            <div class="card-body">
+                                                <h5 class="card-title">Total Post</span></h5>
+
+                                                <div class="d-flex align-items-center">
+                                                    <div
+                                                        class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                                                        <i class="bi bi-file-text-fill"></i>
+                                                    </div>
+                                                    <div class="ps-3">
+                                                        <h6>{{ $totalPosts }}</h6>
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                        </div>
+                                    </div><!-- End Sales Card -->
+
+                                    <!-- Revenue Card -->
+                                    <div class="col-xxl-4 col-md-6">
+                                        <div class="card info-card revenue-card">
+                                            <div class="card-body">
+                                                <h5 class="card-title">Published Post</span></h5>
+
+                                                <div class="d-flex align-items-center">
+                                                    <div
+                                                        class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                                                        <i class="bi bi-file-check-fill"></i>
+                                                    </div>
+                                                    <div class="ps-3">
+                                                        <h6>{{  $publishedPosts  }}</h6>
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                        </div>
+                                    </div><!-- End Revenue Card -->
+
+                                    <!-- Customers Card -->
+                                    <div class="col-xxl-4 col-xl-12">
+
+                                        <div class="card info-card customers-card">
+                                            <div class="card-body">
+                                                <h5 class="card-title">Unpublished Post</span></h5>
+
+                                                <div class="d-flex align-items-center">
+                                                    <div
+                                                        class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                                                        <i class="bi bi-file-x-fill"></i>
+                                                    </div>
+                                                    <div class="ps-3">
+                                                        <h6>{{ $unpublishedPosts }}</h6>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div><!-- End Customers Card -->
+                                </div>
+                            </div>
+
+                        </div>
+                    </section>
                 </div>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -19,9 +19,7 @@ Route::get('/', [PostController::class, 'postIndex'])->name('postIndex');
 
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+    Route::get('/dashboard', [PostController::class, 'postStatus'])->name('dashboard');
 
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
